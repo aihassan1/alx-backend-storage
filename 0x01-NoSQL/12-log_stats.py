88b840e98f4a9cc39c25b274aabe804830cb2614
aihassan1 <abdosruor57@gmail.com>
2024-05-15 16:48:45 +0300

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,26 +2,32 @@
 """ script that provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client["logs"]
-collection = db.nginx
+
+def log_status():
+    """logstatus"""
+    client = MongoClient()
+    db = client["logs"]
+    collection = db.nginx
+
+    number_of_docs = collection.count_documents({})
+    number_of_GET = collection.count_documents({"method": "GET"})
+    number_of_POST = collection.count_documents({"method": "POST"})
+    number_of_PUT = collection.count_documents({"method": "PUT"})
+    number_of_PATCH = collection.count_documents({"method": "PATCH"})
+    number_of_DELETE = collection.count_documents({"method": "DELETE"})
+    number_of_GET_PATH = collection.count_documents(
+        {"path": "/status", "method": "GET"}
+    )
+
+    print(f"{number_of_docs} logs")
+    print("Methods:")
+    print(f"    method GET: {number_of_GET}")
+    print(f"    method POST: {number_of_POST}")
+    print(f"    method PUT: {number_of_PUT}")
+    print(f"    method PATCH: {number_of_PATCH}")
+    print(f"    method DELETE: {number_of_DELETE}")
+    print(f"{number_of_GET_PATH} status check")
 
 
-number_of_docs = collection.count_documents({})
-number_of_GET = collection.count_documents({"method": "GET"})
-number_of_POST = collection.count_documents({"method": "POST"})
-number_of_PUT = collection.count_documents({"method": "PUT"})
-number_of_PATCH = collection.count_documents({"method": "PATCH"})
-number_of_DELETE = collection.count_documents({"method": "DELETE"})
-number_of_GET_PATH = collection.count_documents(
-    {"path": "/status", "method": "GET"})
-
-
-print(f"{number_of_docs} logs")
-print("Methods:")
-print(f"    method GET: {number_of_GET}")
-print(f"    method POST: {number_of_POST}")
-print(f"    method PUT: {number_of_PUT}")
-print(f"    method PATCH: {number_of_PATCH}")
-print(f"    method DELETE: {number_of_DELETE}")
-print(f"{number_of_GET_PATH} status check")
+if __name__ == "__main__":
+    log_status()
