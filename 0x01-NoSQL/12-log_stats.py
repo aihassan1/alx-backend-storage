411e00d1048a7e512f43d903612ca96c5e07f254
aihassan1 <abdosruor57@gmail.com>
2024-05-15 16:46:00 +0300

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,15 +7,13 @@ db = client["logs"]
 collection = db.nginx
 
 
-
 number_of_docs = collection.count_documents({})
 number_of_GET = collection.count_documents({"method": "GET"})
 number_of_POST = collection.count_documents({"method": "POST"})
 number_of_PUT = collection.count_documents({"method": "PUT"})
 number_of_PATCH = collection.count_documents({"method": "PATCH"})
 number_of_DELETE = collection.count_documents({"method": "DELETE"})
-number_of_GET_PATH = collection.count_documents(
-    {"path": "/status", "method": "GET"})
+number_of_GET_PATH = collection.count_documents({"path": "/status", "method": "GET"})
 
 
 print(f"{number_of_docs} logs")
