31d6080f292d234aca977aa940dfe26c9e5c7452
aihassan1 <abdosruor57@gmail.com>
2024-05-15 04:21:22 +0300

    11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,5 +3,15 @@
 
 
 def schools_by_topic(mongo_collection, topic):
+    """
+    Retrieve a list of schools that have a specific topic.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB collection object.
+        topic (str): The topic to search for.
+
+    Returns:
+        pymongo.cursor.Cursor: A cursor object containing the matching schools.
+    """
     query = {"topics": topic}
     return mongo_collection.find(query)
