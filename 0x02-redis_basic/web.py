d1515ca16ec92397f8c2cf6ed8d26e11faeac9e9
aihassan1 <abdosruor57@gmail.com>
2024-05-18 00:38:05 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,7 +8,13 @@ def get_page(url: str) -> str:
     """get_page content"""
     r = redis.Redis()
     key = f"count:{url}"
+    content_key = f"{url}"
     r.incr(key)
-    response = requests.get(url)
 
-    return response.text
+    if r.exists(content_key):
+        return r.get(content_key).decode("utf-8")
+
+    else:
+        response = requests.get(url)
+        r.set(content_key, response.text, 10)
+        return response.text
