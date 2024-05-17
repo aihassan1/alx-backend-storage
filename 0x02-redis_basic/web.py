f39f9155a19f067cd96b88484c20f391c34f7383
aihassan1 <abdosruor57@gmail.com>
2024-05-18 00:13:32 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,11 +3,6 @@
 import requests
 import redis
 
-# from functools import wraps
-
-# (prototype: def get_page(url: str) -> str:). The core of the function is very simple.
-# It uses the requests module to obtain the HTML content of a particular URL and returns it.
-
 
 def get_page(url: str) -> str:
     """get_page content"""
@@ -16,5 +11,5 @@ def get_page(url: str) -> str:
     count = r.incr(key)
     r.expire(key, 10)
     response = requests.get(url)
-    
+
     return response
