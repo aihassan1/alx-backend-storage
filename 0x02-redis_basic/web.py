8e41931a5c28b4b5ecbef3d997de9c8d3ffdbc0c
aihassan1 <abdosruor57@gmail.com>
2024-05-18 00:15:17 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,8 +8,8 @@ def get_page(url: str) -> str:
     """get_page content"""
     r = redis.Redis()
     key = f"count:{url}"
-    count = r.incr(key)
     r.expire(key, 10)
+    count = r.incr(key)
     response = requests.get(url)
 
     return response
